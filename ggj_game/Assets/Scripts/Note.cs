f3faef123c4b3db30592813b2c22c7929a60dda7
K-Straiton <straiton98@gmail.com>
2025-01-25 16:07:54 +0800

    Add prototype note game

---
+++ ggj_game/Assets/Scripts/Note.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class Note : MonoBehaviour
+{
+
+    public bool active = false;
+
+    public bool complete = false;
+
+    public char note = 'c';
+    public KeyCode keybind = KeyCode.A;
+
+    private Renderer rend;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        active = true;
+        rend = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (active && Input.GetKeyDown(keybind)) {
+            Debug.Log("A key was pressed.");
+            rend.material.color = Color.red;
+            complete = true;
+            active = false;
+        }
+    }
+} // class
---
+++ ggj_game/Assets/Scripts/SongSpawn.cs
@@ -0,0 +1,85 @@
+using UnityEngine;
+using UnityEngine.Assertions.Must;
+
+public class SongSpawn : MonoBehaviour
+{
+
+    public string songNotes = "cab";
+
+    public bool complete = false;
+
+    [SerializeField]
+    private GameObject noteReference;
+
+    private GameObject[] noteObjects;
+
+    [SerializeField]
+    private Transform[] positions;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // Create note objects for song
+        noteObjects = new GameObject[songNotes.Length];
+
+        for( int i=0 ; i<songNotes.Length ; i++) {
+            noteObjects[i] = Instantiate(noteReference);
+            
+            char noteName = songNotes[i];
+            KeyCode key = getKey(noteName);
+
+            Note noteObject = noteObjects[i].GetComponent<Note>();
+
+            noteObject.note = noteName;
+            noteObject.keybind = key;
+            noteObject.transform.position = positions[i].position;
+        }
+
+        // Initate first note as active
+        noteObjects[0].GetComponent<Note>().active = true;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private KeyCode getKey(char note)
+    {
+        if(note == 'c') {
+            return KeyCode.A;
+        }
+        else if(note == 'a') {
+            return KeyCode.S;
+        }
+        else {
+            return KeyCode.D;
+        }
+    }
+
+    // Check active states
+    private void checkActive()
+    {
+        bool allComplete = false;
+        bool prevNoteComplete = false;
+
+        for( int i=0 ; i<noteObjects.Length ; i++) {
+            Note noteObject = noteObjects[i].GetComponent<Note>();
+
+            // check if note is complete
+            if(!noteObject.active) {
+                if(prevNoteComplete) {
+                    noteObject.active = true;
+                }
+            }
+            else {
+                if(noteObject.complete) {
+                    prevNoteComplete = true;
+                }
+            }
+        }
+    }
+
+}
